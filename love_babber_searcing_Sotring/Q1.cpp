77d8a4e5cfd6b29d03e5c4a92d41d38583354c88
madhur <madhurchaturvedi2000@gmail.com>
2026-08-01 17:52:54 +0530

    solve first & last occurence in array/vector

--- love_babber_searcing_Sotring/Q1.cpp
+++ love_babber_searcing_Sotring/Q1.cpp
@@ -22,6 +22,27 @@ int firstOccurence(vector<int> &arr,int target){
     }
     return ans;
 }
+int lastOccurence(vector<int> &arr,int target){
+    int s =0;
+    int e = arr.size()-1;
+    int mid = s+(e-s)/2;
+    int ans = -1;
+    
+    while(s<=e){
+        if(arr[mid] == target){
+            ans = mid;
+            s = mid + 1;
+        }
+        else if(arr[mid]>target){
+            e = mid - 1;
+        } 
+        else if(arr[mid]<target){
+            s = mid + 1;
+        }
+        mid = s+(e-s)/2;
+    }
+    return ans;
+}
 
 int main() 
 {
@@ -29,7 +50,9 @@ int main()
     vector<int>v{1,3,4,4,4,4,6,7};
     int target=4;
     int indexOffirstOccurence = firstOccurence(v,target);
+    int indexLastOccurence = lastOccurence(v,target);
     cout<< indexOffirstOccurence << endl;
+    cout<< indexLastOccurence << endl;
     
     return 0;
 }
